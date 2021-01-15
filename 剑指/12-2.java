78c1f23238d29326101dea6308b904a077b6c01a
xvjie1996 <41435406+xvjie1996@users.noreply.github.com>
2021-01-15 16:09:06 +0800

    Create 12-2.java

---
+++ 剑指/12-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        char[] words = word.toCharArray();
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(dfs(board, words, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+    boolean dfs(char[][] board, char[] word, int i, int j, int k) {  //注意dfs的命名，将题目给的条件放在前面，遍历的变量ij放在中间，如果有visited数组就放在最后
+        if(i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) return false;
+        if(k == word.length - 1) return true;
+        board[i][j] = '\0';  //用置为空代替visited数组
+        boolean res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) || 
+                      dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i , j - 1, k + 1);
+        board[i][j] = word[k];
+        return res;
+    }
+}
