8e8cf03e81d0b7ab2d10551e6f0287179807b8d0
xvjie1996 <41435406+xvjie1996@users.noreply.github.com>
2021-01-16 12:36:07 +0800

    Update 15.java

--- 剑指/15.java
+++ 剑指/15.java
@@ -3,7 +3,7 @@ public class Solution {
     public int hammingWeight(int n) {
         int res = 0;
         while(n != 0){
-            res += n & 1; //只二进制最右一位比较，n&1=0，说明最右一位是0
+            res += n & 1; //只二进制最右一位比较，因为1二进制就是00001，n&1=0，说明最右一位是0
             n >>>= 1;  //无符号右移专用>>>
         }
         return res;
